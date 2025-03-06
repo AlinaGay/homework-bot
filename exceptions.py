40372c5c70b5b1a9ebb93f3874e5d89b2448bc07
AlinaGay <gayfullina.a@gmail.com>
2025-03-06 20:07:45 +0100

    Add exceptions

---
+++ exceptions.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+
+class BaseAPIError(Exception):
+    """."""
+
+    @property
+    def code(self):
+        """."""
+        raise NotImplementedError
+
+    @property
+    def message(self):
+        """."""
+        raise NotImplementedError
+
+
+class CodeMessageMixin:
+    """."""
+
+    def __init__(
+            self,
+            code: Optional[str] = None,
+            message: Optional[str] = None
+    ):
+        """."""
+        self.code: Optional[str] = code or self.code
+        self.message: Optional[str] = message or self.message
+
+
+class InvalidResponseError(BaseAPIError):
+    """."""
+
+    code = 'invalid_response'
+    message = 'Invalid response'
+
+
+class InvalidDataError(CodeMessageMixin, BaseAPIError):
+    """."""
+
+    code = 'invalid_data'
+    message = 'Invalid data'
